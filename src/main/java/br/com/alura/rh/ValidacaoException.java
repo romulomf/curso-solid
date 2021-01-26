f42bf488065e5f062ab3e85dddcdd3a5b14c18cb
Rômulo Machado Flores <romuloflores@gmail.com>
2021-01-26 20:17:57 -0300

    Adicionado marcador de serialização à classe ValidacaoException

--- src/main/java/br/com/alura/rh/ValidacaoException.java
+++ src/main/java/br/com/alura/rh/ValidacaoException.java
@@ -2,6 +2,8 @@ package br.com.alura.rh;
 
 public class ValidacaoException extends RuntimeException {
 
+	private static final long serialVersionUID = 3174650726492903435L;
+
 	public ValidacaoException(String mensagem) {
 		super(mensagem);
 	}
